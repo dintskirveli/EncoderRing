a034cfe7567cf9b8acd1cc3b6f41b2a5528f2a75
Daniel Intskirveli <danielintskirveli@getpeek.com>
2012-04-05 19:54:25 -0700

    new tab layout, change to xml layout for encoder tab

--- src/com/nijine/encoderring/EncoderRing.java
+++ src/com/nijine/encoderring/EncoderRing.java
@@ -15,7 +15,7 @@ private long time;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.encoderring);
         
         simple = (EditText)findViewById(R.id.textInput);
         complex = (EditText)findViewById(R.id.textOutput);
