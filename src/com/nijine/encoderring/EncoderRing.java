3227a74eb3e86894f84bc2d613e19f1e86ae1e9a
Dmitriy Loshakov <pt1911@gmail.com>
2012-04-16 01:02:59 -0400

    Finished the key generation algorithm, scrambling algorithm in-progress

--- src/com/nijine/encoderring/EncoderRing.java
+++ src/com/nijine/encoderring/EncoderRing.java
@@ -1,5 +1,7 @@
 package com.nijine.encoderring;
 
+import java.math.BigDecimal;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,7 +25,13 @@ private long time;
 
 
     public void Calculate(View view) {
-    	String input = simple.getText().toString();
+    	int[] array1 = new int[15];
+    	int[] array2 = new int[5];
+    	int key;
+    	String outputText;
+    	
+    	
+    /*	String input = simple.getText().toString();
     	byte[] mutation = input.getBytes();
     	int[] timeArray = new int[20];
     	int i = 0;
@@ -66,7 +74,20 @@ private long time;
     	
     	input = new String(mutation);
     	
-    	complex.setText(input);
+    	complex.setText(input); */
+    	
+    	initializeTime();
+    	array1 = makeArrayofTime(time);
+    	array2 = splitTime(array1);
+    	key = key(array2);
+    	
+    	// outputText = encode(key, simple.getText().toString());
+    	// ^ Scrambling algorithm needs work, encode function is incomplete and not fully functional
+    	
+        outputText = "" + key;
+    	
+    	complex.setText(outputText);
+    	
     }
     
     public void initializeTime() {
@@ -85,20 +106,101 @@ private long time;
     	return arrayofTime;
     }
     
-    public void scrambleTime(int[] arrayofTime) {
-    	double subtotal = 0;
-    	double[] bits = new double[5];
+    public int[] splitTime(int[] arrayofTime) {
+    	int x = 0;
+    	int output[] = new int[5];
     	
-    	for (int i = 0; i < 15; i+=3) {
-    		for (int j = i; j < i + 3; j++) {
-    			subtotal += arrayofTime[j] * Math.pow(10, j - i);
-    		}
-    	bits[i / 3] = subtotal;
+    	
+    	// Initialize output array
+    	for (int i = 0; i < 5; i++) {
+    		output[i] = 0;
     	}
     	
-    	// *continue here*
+        // Fill output array with values accordingly (5 sets of 3-digit values)
+    	for (int i = 0; i < 5; i++) {
+    		output[i] = output[i] + ( arrayofTime[x] * 100 );
+    		x++;
+    		
+    		output[i] = output[i] + ( arrayofTime[x] * 10  );
+    		x++;
+    		
+    		output[i] = output[i] + ( arrayofTime[x] );
+    		x++;
+    	}
+    	
+    	return output;
     }
     
+    public int key(int[] blocks) {
+    	double value = 0;
+    	
+    	// Do some math
+    	value = Math.pow(blocks[0], 5);
+    	value = value / (Math.pow(blocks[1], 4));
+    	value = value / (Math.pow(blocks[2], 3));
+    	value = value / (Math.pow(blocks[3], 2));
+    	value = value / (Math.pow(blocks[4], 1));
+    	
+    	// Turn the math output into a usable number, our "key"
+    	while ((value % 10) < 1) {
+    		value = value * 10;
+    	}
+    	
+    	value = value * Math.pow(10, 7);
+    	
+    	// A little more clean-up (cut off the decimals)
+    	int output = ((int) value);
+    	
+    	return output;
+    }
+    
+    public String encode(int key, String input) {
+    	String output;
+    	int[] numbers = new int[8];
+    	int[] numbers2 = new int[8];
+    	byte[] convert;
+    	int first;
+    	
+    	for (int i = 0; i < 8; i++) {
+    		numbers[i] = key % 10;
+    		key /= 10;
+    	}
+    	
+    	for (int i = 0; i < 8; i++) {
+    		numbers2[i] = numbers[i];
+    	}
+    	
+    	first = numbers2[0];
+    	
+    	for (int i = 0; i < 7; i++) {
+    		numbers2[i] = numbers2[i] * numbers2[i + 1];
+    	}
+    	
+    	numbers2[7] = numbers2[7] * first;
+    	
+    	for (int i = 0; i < 8; i++) {
+    		numbers2[i] = numbers2[i] + (numbers[i] * 2);
+    	}
+    	
+    	convert = input.getBytes();
+    	
+    	
+    	
+    	for (int i = 0; i < convert.length; i++) {
+    		if (i > 7) { i = convert.length; }
+    		else {
+    				convert[i] = (byte) (convert[i] + numbers2[i]); 
+    			if (convert[i] < 33) {
+    				convert[i] = (byte) (convert[i] - numbers2[i]);
+    			} 
+    			
+    		}
+    	}
+    	
+    	output = new String (convert);
+    	
+    	return output;
+    }
 }
 
 	
